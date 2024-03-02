07f37eb57e250cf16d15e2f8a40b009b117abb86
Param Shah <param.sanjay.shah@gmail.com>
2024-03-02 18:24:54 +0530

    ready for hosting

--- mongo.js
+++ mongo.js
@@ -1,25 +1,26 @@
-const express=require("express");
-const app=express();
-const {MongoClient}=require("mongodb");
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
 
 const url = "mongodb+srv://param270604:4vDMjO08tXJpItFh@users.qzwkmmp.mongodb.net/?retryWrites=true&w=majority"; // Use environment variable for MongoDB connection URL
 
-app.get("/",(req,res)=>{
-    const client=new MongoClient(url)
-    client.connect().then(()=>{
-        const db=client.db("music");
-        const collection=db.collection("movies")
-        return collection.insertMany([
-            {name:"alia bhatt"},
-            {name:"dharati shah"}
-        ]
+app.get("/", (req, res) => {
+    const client = new MongoClient(url);
+    client.connect()
+        .then(() => {
+            const db = client.db("music");
+            const collection = db.collection("movies");
+            return collection.insertOne({ name: "param" }); 
+        })
+        .then(() => {
+            client.close();
+            console.log("data inserted successfully");
+            res.send("hey param");
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            res.status(500).send("Error inserting data");
+        });
+});
 
-        )
-    }).then(()=>{
-        client.close()
-    }).then(()=>{
-        console.log("data inserted successfully")
-    })
-})
-
-app.listen(3000)
+app.listen(3000);
